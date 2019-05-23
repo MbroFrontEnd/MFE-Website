c490576303cc3877b698afb7c6e8eecdc6943f89
Lewis Morris <lewis@lewismorris.co.uk>
2019-05-23 22:23:25 +0100

    Add gatsby styled-components plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,16 +1,16 @@
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV || 'development'}`
-})
+});
 
-const clientConfig = require('./client-config')
-const token = process.env.SANITY_READ_TOKEN
+const clientConfig = require('./client-config');
+const token = process.env.SANITY_READ_TOKEN;
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss',
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-sanity',
@@ -22,4 +22,4 @@ module.exports = {
       }
     }
   ]
-}
+};
