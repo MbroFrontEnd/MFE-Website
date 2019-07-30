a4764a90da626ef41d37e7cb672574b255e8baf5
Lewis Morris <lewis@lewismorris.co.uk>
2019-07-30 20:03:53 +0100

    Add netlify forms to call for speakers form

--- web/src/components/CallForSpeakers.js
+++ web/src/components/CallForSpeakers.js
@@ -22,38 +22,53 @@ const StyledLabel = styled.label`
   margin-bottom: 0.5rem;
 `;
 
-const CallForSpeakers = () => (
-  <Section>
-    <StyledHeader>
-      <StyledSvg
-        src={require('../assets/icons/megaphone.svg')}
-        alt="Call For Speakers"
-        width={32}
-        height={32}
-      />
-      <HeadingBravo marginBottom="0">Call For Speakers</HeadingBravo>
-    </StyledHeader>
+const CallForSpeakers = () => {
+  return (
+    <Section>
+      <StyledHeader>
+        <StyledSvg
+          src={require('../assets/icons/megaphone.svg')}
+          alt="Call For Speakers"
+          width={32}
+          height={32}
+        />
+        <HeadingBravo marginBottom="0">Call For Speakers</HeadingBravo>
+      </StyledHeader>
 
-    <Text>
-      We’re always on the look out for speakers, especially those from the
-      Middlesbrough area. If you would like to come along to our friendly meetup
-      and talk about something you have learned or something you have
-      experienced then please complete the form below and we’ll get back to you.
-    </Text>
-    <form>
-      <StyledLabel htmlFor="name">Your Name</StyledLabel>
-      <Input id="name" type="text" name="name" />
-      <StyledLabel htmlFor="email">Email Address</StyledLabel>
-      <Input id="email" type="email" name="email" />
-      <StyledLabel htmlFor="twitter">Twitter Handler</StyledLabel>
-      <Input id="twitter" type="twitter" name="twitter" />
-      <StyledLabel htmlFor="description">
-        Tell us about your talk...
-      </StyledLabel>
-      <Textarea id="description" name="description" rows="5" />
-      <ButtonAlpha type="submit">Submit</ButtonAlpha>
-    </form>
-  </Section>
-);
+      <Text>
+        We’re always on the look out for speakers, especially those from the
+        Middlesbrough area. If you would like to come along to our friendly
+        meetup and talk about something you have learned or something you have
+        experienced then please complete the form below and we’ll get back to
+        you.
+      </Text>
+      <form
+        name="Contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="Contact" />
+        <input type="hidden" name="bot-field" />
+
+        <StyledLabel htmlFor="name">Your Name</StyledLabel>
+        <Input id="name" type="text" name="name" />
+
+        <StyledLabel htmlFor="email">Email Address</StyledLabel>
+        <Input id="email" type="email" name="email" />
+
+        <StyledLabel htmlFor="twitter">Twitter Handler</StyledLabel>
+        <Input id="twitter" type="twitter" name="twitter" />
+
+        <StyledLabel htmlFor="description">
+          Tell us about your talk...
+        </StyledLabel>
+        <Textarea id="description" name="description" rows="5" />
+
+        <ButtonAlpha type="submit">Submit</ButtonAlpha>
+      </form>
+    </Section>
+  );
+};
 
 export default CallForSpeakers;
